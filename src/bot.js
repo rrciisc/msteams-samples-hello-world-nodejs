1f48971954302ed8843189b08cc1f7a448042988
Rooparam Choudhary <roopchou@microsoft.com>
2019-05-21 19:11:50 +0530

    pass commands to bot from 1-1 chat

--- src/bot.js
+++ src/bot.js
@@ -25,9 +25,73 @@ module.exports.setup = function(app) {
     // Define a simple bot with the above connector that echoes what it received
     var bot = new builder.UniversalBot(connector, function(session) {
         // Message might contain @mentions which we would like to strip off in the response
-        var text = teams.TeamsMessage.getTextWithoutMentions(session.message);
-        session.send('You said: %s', text);
+        // var text = teams.TeamsMessage.getTextWithoutMentions(session.message);
+        // session.send('You said: %s', text);
+
+        /*
+        const msg = new teams.TeamsMessage(session).text('This is a test notification message.');
+        // this is a dictionary which could be merged with other properties
+        const alertFlag = teams.TeamsMessage.alertFlag;
+        const notification = msg.sourceEvent({'*': alertFlag});
+
+        // this should trigger an alert
+        session.send(notification);
+        */
+
+       var toMention = {
+        name: 'Rooparam Choudhary',
+        id: 'a784a409-94c6-4341-b765-f36d949c07af'
+        };
+      var msg = new teams.TeamsMessage(session).text(teams.TeamsMessage.getTenantId(session.message));
+      var mentionedMsg = msg.addMentionToText(toMention);
+      var generalMessage = mentionedMsg.routeReplyToGeneralChannel();
+      session.send(generalMessage);
     }).set('storage', inMemoryBotStorage);
+
+    /*
+
+    bot.dialog('/', [
+        session => { builder.Prompts.choice(session, "Choose an option:", 'Route message to general channel|NotificationFeed'); },
+        (session, results) => {
+            switch (results.response.index) {
+                case 0:
+                    session.beginDialog('RouteMessageToGeneral');
+                    break;
+                case 1:
+                    session.beginDialog('NotificationFeed');
+                    break;
+                default:
+                    session.endDialog();
+                    break;
+            }
+        }
+    ]);
+
+    bot.dialog('RouteMessageToGeneral', session => {
+        // user name/user id
+        var toMention = {
+          name: 'Rooparam Choudhary',
+          id: 'a784a409-94c6-4341-b765-f36d949c07af'
+        };
+        var msg = new teams.TeamsMessage(session).text(teams.TeamsMessage.getTenantId(session.message));
+        var mentionedMsg = msg.addMentionToText(toMention);
+        var generalMessage = mentionedMsg.routeReplyToGeneralChannel();
+        session.send(generalMessage);
+        session.endDialog();
+      });
+
+    bot.dialog('NotificationFeed', session => {
+        const msg = new teams.TeamsMessage(session).text('This is a test notification message.');
+        // this is a dictionary which could be merged with other properties
+        const alertFlag = teams.TeamsMessage.alertFlag;
+        const notification = msg.sourceEvent({'*': alertFlag});
+
+        // this should trigger an alert
+        session.send(notification);
+        session.endDialog();
+    });
+
+    */
 
     // Setup an endpoint on the router for the bot to listen.
     // NOTE: This endpoint cannot be changed and must be api/messages
