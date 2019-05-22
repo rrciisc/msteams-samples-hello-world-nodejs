7a7dafc37d58bd3137ad5ffa437dc38285b45bf6
Rooparam Choudhary <roopchou@microsoft.com>
2019-05-22 10:34:25 +0530

    version bump

--- src/bot.js
+++ src/bot.js
@@ -204,7 +204,7 @@ module.exports.setup = function(app) {
         // user name/user id
         var msg = new teams.TeamsMessage(session).text("This is a test notification message.");
         // This is a dictionary which could be merged with other properties
-        var alertFlag = teams.TeamsMessage.AlertFlag();
+        var alertFlag = teams.TeamsMessage.alertFlag;
         var notification = msg.sourceEvent({
             'msteams': alertFlag
         });
